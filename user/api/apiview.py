a80bf51b5ec4c565aa6829d63406e01b26d861f0
surajnai567 <surajnai567@gmail.com>
2021-05-08 01:15:36 +0530

    added forget user and update user

--- user/api/apiview.py
+++ user/api/apiview.py
@@ -5,7 +5,12 @@ from rest_framework.response import Response
 from .serializer import UserSerializer
 import json
 from utils import make_password, check_password
+import random
+from emailsender import EmailSender
 
+temp = {}
+key = ""
+sender = EmailSender.instance(key)
 
 class UserRegisterView(APIView):
 	def post(self, request):
@@ -46,6 +51,44 @@ class UserLogin(APIView):
 class UpdateUser(APIView):
 	def post(self, request):
 		post_data = json.loads(request.body.decode('utf-8'))
-		user = User.objects.filter(token=post_data['token'])[0]
-		serial = UserSerializer(user).data
-		return JsonResponse({"code": 200, "status": "success", "userData": serial})
+		token = post_data['token']
+		user = User.objects.filter(email=post_data['email']).all()
+		if len(user):
+			if token == user[0].token:
+				user[0].fname = post_data['fname']
+				user[0].lname = post_data['lname']
+				user[0].description = post_data['description']
+				user[0].image = post_data['image']
+				user[0].save()
+				return JsonResponse({"code": 200, "status": "success", "userData": {"state": "profile updated successful"}})
+		return JsonResponse({"code": 200, "status": "success", "userData": {"state": "updation failed"}})
+
+
+class ForgetPassword(APIView):
+	def post(self, request):
+		post_data = json.loads(request.body.decode('utf-8'))
+		email = post_data['email']
+		otp = random.randint(100000, 999999)
+		temp[email] = str(otp)
+		sender.send(email, otp)
+		return JsonResponse({"code": 200, "status": "success", "userData": {"state": "sent successful"}})
+
+
+class UpdatePassword(APIView):
+	def post(self, request):
+		post_data = json.loads(request.body.decode('utf-8'))
+		email = post_data['email']
+		otp = post_data['otp']
+		password = post_data['password']
+		if temp.get(email) == otp:
+			user = User.objects.filter(email=email).all()
+			if len(user):
+				user = user[0]
+				user.password = make_password(password.encode())
+				user.save()
+				return JsonResponse({"code": 200, "status": "success", "userData": {"state":"password updated successful"}})
+
+		return JsonResponse({"code": 200, "status": "success", "userData": {"state": "wrong credential please try again"}})
+
+
+
