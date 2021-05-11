6b3e12333e7d13d10f0a3c893f0260fe244b7ff8
surajnai567 <surajnai567@gmail.com>
2021-05-12 04:46:59 +0530

    urls py

--- router/urls.py
+++ router/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from user.api.apiview import UserRegisterView, UserLogin, UpdateUser,ForgetPassword, UpdatePassword
-from event.api.apiview import CreateEventApiView, MyEventApiView, TodayEventApiView
+from user.api.apiview import UserRegisterView, UserLogin, \
+	UpdateUser,ForgetPassword, UpdatePassword, AddFollowers,\
+	MyFollowers,MyFollowing, AddMyAttending
+from event.api.apiview import CreateEventApiView, MyEventApiView,\
+	TodayEventApiView, AllEvents
 
 urlpatterns = [
 	path('register', UserRegisterView.as_view()),
@@ -10,6 +13,11 @@ urlpatterns = [
 	path('update-user', UpdateUser.as_view(),),
 	path('create-event', CreateEventApiView.as_view(),),
 	path('my-event', MyEventApiView.as_view(),),
-	path('today-event', TodayEventApiView.as_view(),)
+	path('today-event', TodayEventApiView.as_view(),),
+	path('add-following', AddFollowers.as_view(),),
+	path('my-follower', MyFollowers.as_view(),),
+	path('my-following', MyFollowing.as_view(),),
+	path('attending-event', AddMyAttending.as_view(),),
+	path('all-event', AllEvents.as_view(),),
 
 ]
