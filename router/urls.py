4aa7d8a37e66fcc1c7261b8c97a5927917a4d70f
surajnai567 <surajnai567@gmail.com>
2021-05-16 01:10:14 +0530

    update api url

--- router/urls.py
+++ router/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from user.api.apiview import UserRegisterView, UserLogin, \
 	UpdateUser,ForgetPassword, UpdatePassword, AddFollowers,\
-	MyFollowers,MyFollowing, AddMyAttending
+	Followers,Following, AddMyAttending, test
 from event.api.apiview import CreateEventApiView, MyEventApiView,\
-	TodayEventApiView, AllEvents
+	TodayEventApiView, AllEvents, event, He
 
 urlpatterns = [
 	path('register', UserRegisterView.as_view()),
@@ -11,13 +11,20 @@ urlpatterns = [
 	path('forget', ForgetPassword.as_view(),),
 	path('update', UpdatePassword.as_view(),),
 	path('update-user', UpdateUser.as_view(),),
-	path('create-event', CreateEventApiView.as_view(),),
+	path('event', CreateEventApiView.as_view(),),
+	path('event/<int:id>', event, ),
+
 	path('my-event', MyEventApiView.as_view(),),
 	path('today-event', TodayEventApiView.as_view(),),
-	path('add-following', AddFollowers.as_view(),),
-	path('my-follower', MyFollowers.as_view(),),
-	path('my-following', MyFollowing.as_view(),),
-	path('attending-event', AddMyAttending.as_view(),),
 	path('all-event', AllEvents.as_view(),),
 
+	path('user/<str:username>/follow', AddFollowers.as_view(),),
+	path('user/<str:username>/followers', Followers.as_view(),),
+	path('user/<str:username>/following', Following.as_view(),),
+
+	path('attending-event', AddMyAttending.as_view(),),
+
+
+	path('a/<str:username>', He.as_view(),),
+	path('', test,),
 ]
