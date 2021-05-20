c1b237b4a3cee4449d260bd1f93a8844aeb4b29e
surajnai567 <surajnai567@gmail.com>
2021-05-20 23:26:02 +0530

    swaggerresponse.py user

---
+++ user/api/swaggerresponse.py
@@ -0,0 +1,53 @@
+from rest_framework import serializers
+
+
+class PostCredentialUser(serializers.Serializer):
+    """Your data serializer, define your fields here."""
+    username = serializers.CharField(required=True)
+    token = serializers.CharField(required=True)
+
+
+class AttendEventParams(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    token = serializers.CharField(required=True)
+    event_id = serializers.IntegerField(required=True)
+
+
+class UpdatePasswordParams(serializers.Serializer):
+    email = serializers.CharField(required=True)
+    otp = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+class ForgetPasswordParams(serializers.Serializer):
+    email = serializers.CharField(required=True)
+
+
+class UserUpdateParams(serializers.Serializer):
+    token = serializers.CharField(required=True)
+    username = serializers.CharField(required=True)
+    fname = serializers.CharField(required=True)
+    lname = serializers.CharField(required=True)
+    email = serializers.CharField(required=True)
+    description = serializers.CharField(required=True)
+
+
+class UserLoginParams(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True)
+
+
+class UserRegisterParmas(serializers.Serializer):
+    email = serializers.CharField(required=True)
+    username = serializers.CharField(required=True)
+    fname = serializers.CharField(required=True)
+    lname = serializers.CharField(required=True)
+    mobile = serializers.CharField()
+    password = serializers.CharField(required=True)
+
+
+
+
+
+
+
